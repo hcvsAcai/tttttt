ee653d7e8d09ed501855b9b33abe17b61d3f536c
hcvsAcai <48270488+hcvsAcai@users.noreply.github.com>
2019-04-25 23:39:46 +0800

    Update 702.c

--- 702.c
+++ 702.c
@@ -3,10 +3,10 @@
 int main () 
 {
     char string[20];
-    double output;
+    int output;
     printf("請輸入一數字的字串: ");
     scanf("%s", string);
     output=atoi(string);
-    printf("%s轉換後的整數為%f\n", string, output);
+    printf("%s轉換後的整數為%d\n", string, output);
     return 0;
 }
